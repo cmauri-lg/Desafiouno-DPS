e83e4a23d01252ce281abbd098821b2f3ba18a15
cmauri-lg <cmauri.lg@gmail.com>
2025-02-28 16:18:45 -0600

    Agregando Factura

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,31 +9,38 @@ export const Headers = ({
   getCountProducts,
   setCountProducts,
 }) => {
-  
   const [getactive, setActive] = useState(false);
-  
+  const [getIsVisible, setIsVisible] = useState(false);
+
   const onDeleteProduct = (product) => {
-    const confirmed = window.confirm('¿Estás seguro de que deseas eliminar este artículo del carrito?');
-    if(confirmed){
-    const results = getAllProducts.filter((item) => item.id !== product.id);
-    
-    setTotal(getTotal - product.price * product.quantity);
-    setCountProducts(getCountProducts - product.quantity);
-    setAllProducts(results);
-  }};
+    const confirmed = window.confirm(
+      "¿Estás seguro de que deseas eliminar este artículo del carrito?"
+    );
+    if (confirmed) {
+      const results = getAllProducts.filter((item) => item.id !== product.id);
+
+      setTotal(getTotal - product.price * product.quantity);
+      setCountProducts(getCountProducts - product.quantity);
+      setAllProducts(results);
+    }
+  };
   const onCleanCart = () => {
-    const confirmed = window.confirm('¿Estás seguro de que vaciar el carrito?');
-    if(confirmed){
-    setAllProducts([]);
-    setTotal(0);
-    setCountProducts(0);
-  }};
+    const confirmed = window.confirm("¿Estás seguro de que vaciar el carrito?");
+    if (confirmed) {
+      setAllProducts([]);
+      setTotal(0);
+      setCountProducts(0);
+    }
+  };
 
   return (
     <header>
       <h1>Tienda de Tecnologia</h1>
       <div className="container-icon">
-        <div className="container-cart-icon" onClick={() => setActive(!getactive)}>
+        <div
+          className="container-cart-icon"
+          onClick={() => setActive(!getactive)}
+        >
           <img
             src="https://w7.pngwing.com/pngs/275/763/png-transparent-cart-shopping-supermarket-shopping-cart-ecommerce-e-commerce-shopping-trolley-caddy.png"
             alt="carrito"
@@ -44,36 +51,85 @@ export const Headers = ({
           </div>
         </div>
 
-        <div className={`container-cart-products ${ getactive ?'':'hidden-cart'}`}>
-        {getAllProducts.length ? ( 
-          <>
-          <div className="row-product">
-            {getAllProducts.map(product =>(
-              <div className='cart-product' key = {product.id}>
-              <div className='info-cart-product'>
-                <figure className="tamanho">
-                  <img className="tamanho" src={product.urlImage} />
-                </figure>
-                <span className='cantidad-producto-carrito'> {product.quantity} </span>
-                <p className='titulo-producto-carrito'> {product.title} </p> 
-                <span className='precio-producto-carrito'> ${product.price} </span> 
-                </div>
-              <img
-                src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png"
-                alt="cerrar"
-                className="icon-close"
-                onClick = {()=> onDeleteProduct(product)}
-              />
-            </div>
-             ))}
-          </div>
+        <div
+          className={`container-cart-products ${
+            getactive ? "" : "hidden-cart"
+          }`}
+        >
+          {getAllProducts.length ? (
+            <>
+              <div className="row-product">
+                {getAllProducts.map((product) => (
+                  <div className="cart-product" key={product.id}>
+                    <div className="info-cart-product">
+                      <figure className="tamanho">
+                        <img className="tamanho" src={product.urlImage} />
+                      </figure>
+                      <span className="cantidad-producto-carrito"> 
+                        {product.quantity}
+                      </span>
+                      <p className="titulo-producto-carrito">
+                        {product.title}
+                      </p>
+                      <span className="precio-producto-carrito">
+                        ${product.price}
+                      </span>
+                    </div>
+                    <img
+                      src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png"
+                      alt="cerrar"
+                      className="icon-close"
+                      onClick={() => onDeleteProduct(product)}
+                    />
+                  </div>
+                ))}
+              </div>
+              <button className="btn-clear-all" onClick={onCleanCart}>
+                
+                Vaciar Carrito
+              </button>
+            </>
+          ) : (
+            <p className="cart-empty">El carrito está vacío</p>
+          )}
 
-          <div className="cart-total">
-            <h3>Total:</h3>
-            <span className="total-pagar">${getTotal}</span>
-          </div>
+          <hr></hr>
 
-          <button className='btn-clear-all' onClick={onCleanCart}> Vaciar Carrito </button> </> ) : ( <p className='cart-empty'>El carrito está vacío</p> )}
+          <div className="p-4">
+            <button
+              onClick={() => setIsVisible(!getIsVisible)}
+              className="btn-clear-all"
+            >
+              {getIsVisible ? "OCULTAR FACTURA" : "COMPRAR"}
+            </button>
+            {getIsVisible && (
+              <pre className="mt-4 bg-gray-200 rounded">
+                {
+                  <div className="cart">
+                    <h2>Factura Digital</h2>
+                    {getAllProducts.map((product) => (
+                      <div className="cart" key={product.id}>
+                        <span className="text-personalizado">Cantidad:</span>
+                        <span className="cantidad-producto-carrito">
+                          {product.quantity}
+                        </span>
+                        <p className="text-personalizado">Nombre descriptivo del producto:</p>
+                        <p className="titulo-producto-carrito">
+                          {product.title}
+                        </p>
+                        <span className="text-personalizado">Precio Unitario:</span>
+                        <span className="precio-producto-carrito">
+                          ${product.price}
+                        </span>
+                      </div>
+                    ))}
+                    <h3>Total:</h3>
+                    <span className="total-pagar">${getTotal.toFixed(2)}</span>
+                  </div>
+                }
+              </pre>
+            )}
+          </div>
         </div>
       </div>
     </header>
