2842bd87bdaefcce32eac5239f979091b9725c29
cmauri-lg <cmauri.lg@gmail.com>
2025-02-27 23:20:02 -0600

    agregando el data.js y datos de prueba

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    id: 1,
+    title: "Case midtower aerocool d502a-g-bk-v1 negro tempered glass 4 fan argb",
+    price: 99.95,
+    urlImage: "https://api.zonadigitalsv.com/storage/products/imagen_generada67c0f2e494370.jpg",
+    quantity: 3,
+  },
+  {
+    id: 2,
+    title: "Auriculares tws bluetooth xiaomi redmi buds 6 pro morado 59568 con anc hi-res audio",
+    price: 89.95,
+    urlImage:
+      "https://api.zonadigitalsv.com/storage/products/imagen_generada67c095e78696c.jpg",
+    quantity: 5,
+  },
+];
