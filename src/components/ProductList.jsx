30300027deb5abd08d9c8b3d210cb2ca508a2d3e
cmauri-lg <cmauri.lg@gmail.com>
2025-02-28 10:22:53 -0600

    Modificacion del ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import { data } from "../data.js";
-export const ProductList = () => {
+
+export const ProductList = ({
+  getAllProducts,
+  setAllProducts,
+  getTotal,
+  setTotal,
+  getCountProducts,
+  setCountProducts,}
+) => {
+
+  const onAddProduct = product =>{
+    if (getAllProducts.find(item => item.id === product.id)) {
+      const products = getAllProducts.map(item => 
+        item.id === product.id 
+        ? {...item, quantity : item.quantity + 1}
+        :item
+      );
+        setTotal(getTotal + product.price * product.quantity); 
+        setCountProducts(getCountProducts + product.quantity); 
+        return setAllProducts([...products]);
+    }
+
+    setTotal(getTotal + product.price * product.quantity); 
+    setCountProducts(getCountProducts + product.quantity); 
+    setAllProducts([...getAllProducts, product]);
+  };
+
   return (
     <div className="container-items">
-      {data.map((product) => (
-        <div className="item" key={product.id}>
+      {data.map(product => (
+        <div className='item' key={product.id}>
           <figure>
             <img src={product.urlImage} alt={product.title} />
           </figure>
           <div className="info-product">
-            <h2 className="titulo-producto-carrito ">{product.title}</h2>
-            <p className="price">${product.price}</p>
-            <button className="btn-add-cart">Añadir al carrito</button>
+            <h2>{product.nameProduct}</h2>
+            <p className='price'>${product.price}</p>
+            <button onClick={()=>onAddProduct(product)}>Añadir al carrito</button>
           </div>
         </div>
       ))}
