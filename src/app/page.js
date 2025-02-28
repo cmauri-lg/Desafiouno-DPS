a69de082e3a89a9c1d38adec917d3cc4cc014e15
cmauri-lg <cmauri.lg@gmail.com>
2025-02-28 10:01:40 -0600

    Funcionalidad al Carrito en page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,32 @@
+"use client"
+import { useState } from "react";
 import { Headers } from "../components/Header.jsx";
 import { ProductList } from "../components/ProductList.jsx";
 
 export default function Home() {
+  const [getAllProducts, setAllProducts] = useState([]);
+  const [getTotal, setTotal] = useState(0);
+  const [getCountProducts, setCountProducts] = useState(0);
+
+
   return (
     <>
-      <Headers></Headers>
-      <ProductList></ProductList>
+      <Headers
+      getAllProducts = {getAllProducts}
+      setAllProducts ={setAllProducts}
+      getTotal = {getTotal}
+      setTotal = {setTotal}
+      getCountProducts = {getCountProducts}
+      setCountProducts = {setCountProducts}  
+      />
+      <ProductList
+      getAllProducts = {getAllProducts}
+      setAllProducts ={setAllProducts}
+      getTotal = {getTotal}
+      setTotal = {setTotal}
+      getCountProducts = {getCountProducts}
+      setCountProducts = {setCountProducts}  
+      />
     </>
   );
 }
